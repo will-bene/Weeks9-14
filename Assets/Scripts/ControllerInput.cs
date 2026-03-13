e63f91e1585b88044971f55ba9ec317b7605268c
beneteaw <beneteaw@OA-S141-18>
2026-03-13 17:53:56 -0400

    Week 9 - Coding gym 1

--- Assets/Scripts/ControllerInput.cs
+++ Assets/Scripts/ControllerInput.cs
@@ -5,17 +5,22 @@ public class ControllerInput : MonoBehaviour
 {
     public float speed;
     public Vector2 directionalInput;
+    public Vector2 lookInput;
+    public Vector2 pointInput;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
         transform.position += (Vector3)directionalInput * speed * Time.deltaTime;
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(pointInput);
+        mousePosition.z = 0;
+        transform.up = mousePosition-transform.position;
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -24,11 +29,16 @@ public class ControllerInput : MonoBehaviour
     }
     public void OnAttack(InputAction.CallbackContext context)
     {
-        Debug.Log("Attack Time! ( "+ context.phase +" )");
+        Debug.Log("Attack Time! ( " + context.phase + " )");
     }
 
     public void OnPoint(InputAction.CallbackContext context)
     {
         Debug.Log("On point ( " + context.ReadValue<Vector2>() + " )");
+        pointInput = context.ReadValue<Vector2>();
+    }
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        //lookInput = context.ReadValue<Vector2>();
     }
 }
