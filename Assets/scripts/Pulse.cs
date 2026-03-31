184c48506d142d6d2790e0524f1ab2be9fc192fb
Will Beneteau <flyingfairy33@gmail.com>
2026-03-31 19:12:17 -0400

    Week 11 - Coding Gym part 2

--- Assets/scripts/Pulse.cs
+++ Assets/scripts/Pulse.cs
@@ -5,6 +5,7 @@ public class Pulse : MonoBehaviour
     public float speed = 2;
     public AnimationCurve curve;
     public float pulseProgress=0;
+    public TrailRenderer trail;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -16,15 +17,25 @@ public class Pulse : MonoBehaviour
     {
         //transform.position += new Vector3(speed, 0, 0)*Time.deltaTime;
         pulseProgress += speed * Time.deltaTime;
-        float xValue = pulseProgress;
-        float yValue = curve.Evaluate(pulseProgress);
+        float xProgValue = pulseProgress;
+        float yProgValue = curve.Evaluate(pulseProgress);
 
-        if (pulseProgress>1)
+        if (pulseProgress > 1)
         {
+            trail.Clear();
+            trail.emitting = false;
             pulseProgress = 0;
         }
+        else
+        {
+            trail.emitting = true;
+        }
 
+        float xValue = Screen.width * xProgValue;
+        float yValue = Screen.height*yProgValue;
 
-
+        Vector3 newPosition = Camera.main.ScreenToWorldPoint( new Vector3(xValue, yValue, 0));
+        newPosition.z = 0;
+        transform.position = newPosition;
     }
 }
