7f87e27b8b9d72afc1470c002ff4781066b20d12
Will Beneteau <flyingfairy33@gmail.com>
2026-04-04 01:20:45 -0400

    Challenge 2 - Watering / Coroutine Control - Completed

--- Assets/scripts/System Scripts/Flower.cs
+++ Assets/scripts/System Scripts/Flower.cs
@@ -1,52 +1,116 @@
-using UnityEngine;
+using System.Collections.Generic;
+using System;
 using System.Collections;
+using System.Linq;
 using UnityEditor;
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UI;
 public class Flower : MonoBehaviour
 {
+    //gorCurve
     public AnimationCurve curve;
+
+    //growing
     public float duration = 10;
-    private float growProgress = 0f;
+    public float growProgress = 0f; //from 0-1
+    public List<float> partsProgress; //contains progress of parts
     private int partsAmount = 3;
-    private float waterValue = 0.5f;
+
+    //water
+    private float waterDuration = 10f;
+    public float waterValue = 5f;
+    public Slider waterSlider;
+
+    //grow coroutine storage
+    public Coroutine growCoroutine;
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        waterSlider.maxValue = waterDuration; //initialize water slider maximum
 
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        HandleWater(); //decrease water value, stopping grow coroutine if its low enough
     }
 
-    public float getPartProgress(int curPart)
-    {//using the 'global' flower grow progress, get the individual progress based on which part youre on
-        float curGlobalProg = growProgress * partsAmount; //gets global progress from 0-3
-        float partProg = 1-Mathf.Clamp((curPart+1) -curGlobalProg, 0, 1); //gets current part's progress, ex if progress is at 0.5 -> 1.5, part 1 is at 1, part 2 at 0.5, part 3 at 0
-        return partProg; //returns progress of part from 0-1
+    public void Initialize()
+    {
+        for (int i = 0; i < partsAmount; i++)
+        {//initialize part progress
+            partsProgress.Add(0);
+            //Debug.Log(partsProgress[0]);
+        }
     }
+
+    public void updateGlobalProgress()
+    {//updates global progress (combining all part progress into one value) from 0-1
+        float curAmount = partsProgress.Sum(); //from 0-3
+        //Debug.Log(curAmount);
+        growProgress = (curAmount / partsAmount) / duration;
+    }
+
     public IEnumerator GrowParts()
     {//coroutine for growing each part of flower based on grow progress
         for (int i = 0; i < partsAmount; i++)
         {//set grow scale of each child to current progress amount (only really matters when coroutine is restarted, otherwise will start at x0 scale)
             Transform curChild = transform.GetChild(i); //get child at i
-            curChild.localScale = Vector3.one * (getPartProgress(i)); //set child's scale based on global prog
+
+            curChild.localScale = (Vector3.one * curve.Evaluate(partsProgress[i] / duration)); //set child's scale based on global prog
         }
 
         for (int i = 0; i < partsAmount; i++)
         {//loop through each child piece
-            float progress = getPartProgress(i)*duration; //reset progress to match global grow progress
+            //float progress = partsProgress[i]; //reset progress to match global grow progress
 
             Transform curChild = transform.GetChild(i); //get child at i
-            while (progress < duration)
+            while (partsProgress[i] < duration)
             {//until progress is maxxed out
-                progress += Time.deltaTime; //increase progress
-                curChild.localScale = (Vector3.one * curve.Evaluate(progress / duration)); //set child's scale based on prog
+                partsProgress[i] += Time.deltaTime; //increase progress
+
+                updateGlobalProgress(); //update global grow progress
+
+                curChild.localScale = (Vector3.one * curve.Evaluate(partsProgress[i] / duration)); //set child's scale based on prog
                 yield return null; //point of coroutine pause
             }
         }
 
+    }
+
+    public void HandleWater()
+    {//always active, doesn't need to be in a coroutine
+        if (growProgress < 1)
+        {//not finished growing yet
+            if (waterValue > 0)
+            {//when water is above 0
+                waterValue -= Time.deltaTime; //decrease water value
+                waterSlider.value = waterValue;//change slider to match value
+            }
+            else
+            {//stop growing coroutine when out of water
+                if (growCoroutine != null)
+                {//check if it exists first
+                    StopCoroutine(growCoroutine); //stop growing
+                }
+            }
+        }
+    }
+
+    public void WaterFlower()
+    {//water flower, reset its value to duration
+        waterValue = waterDuration;
+        waterSlider.value = waterValue;
+
+        if (growCoroutine != null) 
+        {//if grow routine is already active, restart it
+            StopCoroutine(growCoroutine); //stop growing
+            growCoroutine = StartCoroutine(GrowParts());
+        }
     }
 
 }
--- Assets/scripts/System Scripts/FlowerSpawner.cs
+++ Assets/scripts/System Scripts/FlowerSpawner.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UIElements;
@@ -5,6 +6,8 @@ using UnityEngine.UIElements;
 public class FlowerSpawner : MonoBehaviour
 {
     public GameObject spawnPrefab;
+    public List<GameObject> spawnedFlowers;
+    //public Vector3 mousePos;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -14,13 +17,67 @@ public class FlowerSpawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        mousePos.z = 0;
-        if (Mouse.current.leftButton.wasPressedThisFrame)
+        
+    }
+
+    public void OnPoint(InputAction.CallbackContext context)
+    {
+        //-- set position to mouse position (in world space) --
+        Vector3 newPos = Vector3.zero;
+        newPos= Camera.main.ScreenToWorldPoint(context.ReadValue<Vector2>());
+        newPos.z = 0;
+        transform.position = newPos;
+    }
+
+    public void OnClick(InputAction.CallbackContext context)
+    {
+        if (context.performed)
         {
-            GameObject newFlower = Instantiate(spawnPrefab, mousePos, Quaternion.identity);
-            Flower flowerScript = newFlower.GetComponent<Flower>();
-            flowerScript.StartCoroutine(flowerScript.GrowParts());
+            //-- spawn a new flower prefab in current position, starting its coroutine --
+            SpawnFlower();
         }
     }
+
+    public void SpawnFlower()
+    {
+        GameObject newFlower = Instantiate(spawnPrefab, transform.position, Quaternion.identity); //spawn a flower
+        spawnedFlowers.Add(newFlower); //add new flower to list
+
+        Flower flowerScript = newFlower.GetComponent<Flower>();
+        flowerScript.Initialize(); //must be initialized before starting coroutine
+        flowerScript.growCoroutine = flowerScript.StartCoroutine(flowerScript.GrowParts()); //start coroutine
+        
+    }
+
+    public void OnRightClick(InputAction.CallbackContext context)
+    {
+        if (context.started == true)
+        {
+            //-- 
+            WaterFlower();
+        }
+    }
+    public void WaterFlower()
+    {
+        float waterRange = 1f;
+        GameObject closestFlower = null;
+        //loop through all flowers, checking the closest and storing it as the closest
+        for (int i = 0; i < spawnedFlowers.Count; i++)
+        {
+            GameObject curFlower = spawnedFlowers[i]; //get current check
+            float curDistance = Vector3.Distance( curFlower.transform.position, transform.position ); //get current distance
+            if (curDistance < waterRange)
+            {//its closer than the last one
+                waterRange = curDistance; //lower range threshold
+                closestFlower = curFlower; //set current check as closest
+            }
+        }
+
+        if (closestFlower != null)
+        {//if there is a close flower
+            Flower flowerScript = closestFlower.GetComponent<Flower>();
+            flowerScript.WaterFlower();
+        }
+    }
+
 }
